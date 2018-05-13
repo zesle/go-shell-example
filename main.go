2d948c7ffb8e2529d8030a0f265234b5bb334000
Simon Jürgensmeyer <Simon.Juergensmeyer@gmail.com>
2018-05-13 16:45:14 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("> ")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+		}
+		input = strings.TrimSuffix(input, "\n")
+
+		args := strings.Split(input, " ")
+
+		if len(args) < 1 {
+			args = append(args, "")
+		}
+		cmd := exec.Command(args[0], args[1:]...)
+		stdoutStderr, err := cmd.CombinedOutput()
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Printf("%s\n", stdoutStderr)
+	}
+}
